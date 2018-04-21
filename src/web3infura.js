3bbd86bf3a712ed091edc8c895f0b65b54896cae
akinyemi <akbumper@gmail.com>
2018-04-21 11:40:13 +0100

    https websocket

--- src/web3infura.js
+++ src/web3infura.js
@@ -4,7 +4,7 @@
 import Web3 from 'web3';
 
 
-var ws_provider = 'ws://ropsten.infura.io/LSyzSip32kjqWW2EXh4N/ws/'
+var ws_provider = 'wss://ropsten.infura.io/LSyzSip32kjqWW2EXh4N/ws/'
     
 //overrides metamask v0.2 for our v 1.0
 const web3 = new Web3(new Web3.providers.WebsocketProvider(ws_provider));
